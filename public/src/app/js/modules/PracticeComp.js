b405c4e04d42b2be44502599f7cdc0cbc0d7d685
Logan <lwheeler1@carthage.edu>
2017-12-09 12:47:06 -0600

    saving on my laptop to switch to my pc

--- public/src/app/js/modules/PracticeComp.js
+++ public/src/app/js/modules/PracticeComp.js
@@ -6,105 +6,97 @@ const velocity = require('velocity-react');
 export class PracticeComp extends React.Component {
 	constructor(props) {
 		super(props)
-		this.state = {buildingsState:"closed", classesState:"closed", peopleState:"closed"}
+		//this.state = {buildingsState:"open", classesState:"closed", peopleState:"open"};
+		this.state = {menuArrowArr:["one", "two"]};
 		this.renderBuildingSubMenu = this.renderBuildingSubMenu.bind(this);
 		this.renderClassesSubMenu = this.renderClassesSubMenu.bind(this);
 		this.renderPeopleSubMenu = this.renderPeopleSubMenu.bind(this);
-		let isMobile = false
-		if (window.innerWidth < 770) {
-			isMobile = true
-		}
-
-		this.state = {
-			isMobile: isMobile,
-			menuItemIsActive: 1,
-			sliderPos: 0,
-		}
 	}
-	renderBuildingSubMenu() {
-		if (this.state.buildingsState=="open")
+
+	renderBuildingSubMenu(string) {
+		if (this.state.menuArrowArr.includes("one"))
 		{
 			return(
-				
 					<h3>
-						<label className="container">
-		  				<input type="checkbox" />
+						<label className="container" for="XXXstring">
+		  				<input type="checkbox" name="XXXstring" />
 		  				<span className="checkmark"></span>
-						</label>				
-						<span className="mapMenuText">Building option #1</span>
-						<img src="closemenu.png" alt="arrow" height="42" width="42" className="mapMenuArrow" /> 				
+							<span className="mapMenuText">Building option #1</span>
+						</label>
+						<img src="closemenu.png" alt="arrow" height="42" width="42" className="mapMenuArrow" />
 					</h3>
 			)
 		}
 	}
 
 	renderClassesSubMenu() {
-		if (this.state.classesState=="open")
-		{
+		//if (this.state.classesState==="open")
+		//{
 			return(
-				
 					<h3>
 						<label className="container">
 		  				<input type="checkbox" />
 		  				<span className="checkmark"></span>
-						</label>				
+						</label>
 						<span className="mapMenuText">Classes option #1</span>
-						<img src="closemenu.png" alt="arrow" height="42" width="42" className="mapMenuArrow" /> 				
+						<img src="closemenu.png" alt="arrow" height="42" width="42" className="mapMenuArrow" />
 					</h3>
 			)
-		}
+		//}
 	}
 
 	renderPeopleSubMenu() {
-		if (this.state.peopleState=="open")
-		{
+		//if (this.state.peopleState==="open")
+		//{
 			return(
-				
 					<h3>
 						<label className="container">
 		  				<input type="checkbox" />
 		  				<span className="checkmark"></span>
-						</label>				
-						<span className="mapMenuText">Classes option #1</span>
-						<img src="closemenu.png" alt="arrow" height="42" width="42" className="mapMenuArrow" /> 				
+						</label>
+						<span className="mapMenuText">People option #1</span>
+						<img src="closemenu.png" alt="arrow" height="42" width="42" className="mapMenuArrow" />
 					</h3>
 			)
-		}
+		//}
 	}
 
 	render() {
 		return (
 		<div className="mapMenu">
 				<div className="mapMenuSearchBar"><input type="text" name="search" placeholder="Search..." /></div>
-				
-				
-			<h2> 
-				<label className="container">
-  				<input type="checkbox" />
+
+			<h2>
+				<label className="container" for='check1'>
+  				<input type="checkbox" name='check1' />
   				<span className="checkmark"></span>
-				</label>				
-				<span className="mapMenuText">{this.props.menuItems[0]}</span>
-				<img src="closemenu.png" alt="arrow" height="42" width="42" className="mapMenuArrow" /> 
+					<span className="mapMenuText">{this.props.menuItems[0]}</span>
+				</label>
+
+				<img src="closemenu.png" alt="arrow" height="42" width="42" className="mapMenuArrow" id="one" />
 			</h2>
 			{this.renderBuildingSubMenu()}
-			<h2> 
-				<label className="container">
-  				<input type="checkbox" />
+			<h2>
+				<label className="container" for='check2'>
+  				<input type="checkbox" name='check2' />
   				<span className="checkmark"></span>
+					<span className="mapMenuText">{this.props.menuItems[1]}</span>
 				</label>
-				<span className="mapMenuText">{this.props.menuItems[1]}</span>
-				<img src="closemenu.png" alt="arrow" height="42" width="42" className="mapMenuArrow" /> 
+				<img src="closemenu.png" alt="arrow" height="42" width="42" className="mapMenuArrow" />
 			</h2>
 			{this.renderClassesSubMenu()}
-			<h2> 
-				<label className="container">
-  				<input type="checkbox" />
+			<h2>
+				<label className="container" for='check3'>
+  				<input type="checkbox" name='check3' />
   				<span className="checkmark"></span>
+					<span className="mapMenuText">{this.props.menuItems[2]}</span>
 				</label>
-				<span className="mapMenuText">{this.props.menuItems[2]}</span>
-				<img src="closemenu.png" alt="arrow" height="42" width="42" className="mapMenuArrow" /> 
+
+				<img src="closemenu.png" alt="arrow" height="42" width="42" className="mapMenuArrow" />
 			</h2>
+
 			{this.renderPeopleSubMenu()}
+
 			<div className="mapMenuButton"><button> Make A Rating</button></div>
 		</div>
 		)
